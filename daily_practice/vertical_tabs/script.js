15fa0096fcea4b234b42ea633fd428e695263b13
elaineapp082020 <joyfulelaine7@gmail.com>
2020-12-03 15:27:37 -0800

    add daily practice - vertical tabs

---
+++ daily_practice/vertical_tabs/script.js
@@ -0,0 +1,16 @@
+function openTab(evt, idName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("job-btn");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(idName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("open");
