dd7af77d5623fc233f7b4b8c74936f9dc809c9f7
elaineapp082020 <joyfulelaine7@gmail.com>
2020-12-29 21:39:19 -0800

    update homepage

---
+++ russell_web/README.md
@@ -0,0 +1,11 @@
+# Russell Leong Design
+We are a design office that producess appropriate and meaningful brand identity graphics for a multitude of clients, small and large: high tech, and soft tech.
+
+## Introduction
+This is a project that I volunteered to build the design studio website for Russell. Currently, only basic HTML,CSS, and JavaScript are used to implement the UI/UX design, and then all components will be rewrite with React to make the code be reusable.
+
+## Technologies
+Project is created with:
+* HTML5
+* CSS3
+* JavaScript
--- russell_web/js/script.js
+++ russell_web/js/script.js
@@ -6,25 +6,12 @@ function closeNav() {
   document.getElementById("overlay").style.width = "0%";
 }
 
+
+
 $(document).ready(function() {
 
-  /* Menu */
-  /*$(".menu-btn a").click(function () {
-      $(".overlay").fadeToggle(200);
-      $(this).toggleClass('btn-open').toggleClass('btn-close');
-  });
-
-  $('.overlay-content').on('click', function () {
-      $(".overlay-content").fadeToggle(200);
-      $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
-  });
-
-  $('.menu-item a').on('click', function () {
-      $(".overlay-content").fadeToggle(200);
-      $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
-  });*/
-
   /* Contact */
+  /*https://formspree.io/*/
   $('.submit').click(function (event) {
     console.log('Click')
 
@@ -66,3 +53,50 @@ $(document).ready(function() {
 
   })
 });
+
+
+function changeDot() {
+  const scrollValue = $(window).scrollTop();
+  const heightSec2 = $('.about').offset().top;
+  const heightSec3 = $('.logo').offset().top;
+  const heightSec4 = $('.print').offset().top;
+  const heightSec5 = $('.vi').offset().top;
+  const heightSec6 = $('.mail').offset().top;
+  const heightSec7 = $('.contact').offset().top;
+  const heightSec8= $('.footer').offset().top;
+
+  if (scrollValue < heightSec2) {
+      $('aside li').not('.bar1').removeClass('active');
+      $('.bar1').addClass('active');
+  } else if (scrollValue < heightSec3) {
+      $('aside li').not('.bar2').removeClass('active');
+      $('.bar2').addClass('active');
+  } else if (scrollValue < heightSec4) {
+      $('aside li').not('.bar3').removeClass('active');
+      $('.bar3').addClass('active');
+  } else if (scrollValue < heightSec5) {
+      $('aside li').not('.bar4').removeClass('active');
+      $('.bar4').addClass('active');
+  } else if (scrollValue < heightSec6) {
+      $('aside li').not('.bar5').removeClass('active');
+      $('.bar5').addClass('active');
+  } else if (scrollValue < heightSec7) {
+      $('aside li').not('.bar6').removeClass('active');
+      $('.bar6').addClass('active');
+  } else if (scrollValue < heightSec8) {
+      $('aside li').not('.bar7').removeClass('active');
+      $('.bar7').addClass('active');
+  } else {
+      $('aside li').not('.bar8').removeClass('active');
+      $('.bar8').addClass('active');
+  }
+}
+
+$(window).on("scroll", changeDot)
+
+$('aside li').on('click', function () {
+  const goToSection = '.s' + $(this).attr('id');
+  $('body, html').animate({
+      scrollTop: $(goToSection).offset().top + 1
+  })
+})
