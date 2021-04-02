cbc439ce59ad992ac4fb9e987bc7c48f4be7d853
elaineapp082020 <joyfulelaine7@gmail.com>
2021-04-01 23:42:07 -0700

    update visual details - icons transition & right side bar

--- russell_web_v3/js/script.js
+++ russell_web_v3/js/script.js
@@ -7,6 +7,17 @@ function closeNav() {
 }
 
 function changeDot() {
+
+  let rightNavLine;
+
+  if ($("#line-index")[0]){
+    rightNavLine = "line-index";
+  } else if ($("#line-about")[0]) {
+    rightNavLine = "line-about";
+  }
+
+  var lineHeight = document.getElementById(rightNavLine);  
+
   const scrollValue = $(window).scrollTop();
   const heightSec2 = $('#section2').offset().top;
   const heightSec3 = $('#section3').offset().top;
@@ -24,38 +35,196 @@ function changeDot() {
   const progress6 = (scrollValue - heightSec6) / (heightSec7 - heightSec6) * 100;
   const progress7 = (scrollValue - heightSec7) / (heightSec8 - heightSec7) * 100;
 
-  if (scrollValue < heightSec2) {
-    $('aside li').not('.bar1').removeClass('rightNav-line');
-    $('.bar1').addClass('rightNav-line');
-    $('.rightNav-line').css({"transform":"translateY(" + progress1 + "%)"});
+  const curSec2Height = document.getElementById("section2").getBoundingClientRect().top;
+  const curSec5Height = document.getElementById("section5").getBoundingClientRect().top;
+  const curSec5BotHeight = document.getElementById("section5").getBoundingClientRect().bottom;
+  const curSec6Height = document.getElementById("section6").getBoundingClientRect().top;
+  const curSec7Height = document.getElementById("section7").getBoundingClientRect().top;
+  const curSec7BotHeight = document.getElementById("section7").getBoundingClientRect().bottom;
+
+  console.log(progress1);
+  /*if (scrollValue < heightSec2) {
+    $('aside li').not('.bar1').removeClass(rightNavLine);
+    $('.bar1').addClass(rightNavLine);
+    $('.' + rightNavLine).css({"transform":"translateY(" + progress1 + "%)"});
   } else if (scrollValue < heightSec3) {
-    $('aside li').not('.bar2').removeClass('rightNav-line');
-    $('.bar2').addClass('rightNav-line');
-    $('.rightNav-line').css({"transform":"translateY(" + progress2 + "%)"});
+    $('aside li').not('.bar2').removeClass(rightNavLine);
+    $('.bar2').addClass(rightNavLine);
+    $('.' + rightNavLine).css({"transform":"translateY(" + progress2 + "%)"});    
   } else if (scrollValue < heightSec4) {
-    $('aside li').not('.bar3').removeClass('rightNav-line');
-    $('.bar3').addClass('rightNav-line');
-    $('.rightNav-line').css({"transform":"translateY(" + progress3 + "%)"});
+    $('aside li').not('.bar3').removeClass(rightNavLine);
+    $('.bar3').addClass(rightNavLine);
+    $('.' + rightNavLine).css({"transform":"translateY(" + progress3 + "%)"});
   } else if (scrollValue < heightSec5) {
-    $('aside li').not('.bar4').removeClass('rightNav-line');
-    $('.bar4').addClass('rightNav-line');
-    $('.rightNav-line').css({"transform":"translateY(" + progress4 + "%)"});
+    $('aside li').not('.bar4').removeClass(rightNavLine);
+    $('.bar4').addClass(rightNavLine);
+    $('.' + rightNavLine).css({"transform":"translateY(" + progress4 + "%)"});
   } else if (scrollValue < heightSec6) {
-    $('aside li').not('.bar5').removeClass('rightNav-line');
-    $('.bar5').addClass('rightNav-line');
-    $('.rightNav-line').css({"transform":"translateY(" + progress5 + "%)"});
+    $('aside li').not('.bar5').removeClass(rightNavLine);
+    $('.bar5').addClass(rightNavLine);
+    $('.' + rightNavLine).css({"transform":"translateY(" + progress5 + "%)"});
   } else if (scrollValue < heightSec7) {
-    $('aside li').not('.bar6').removeClass('rightNav-line');
-    $('.bar6').addClass('rightNav-line');
-    $('.rightNav-line').css({"transform":"translateY(" + progress6 + "%)"});
+    $('aside li').not('.bar6').removeClass(rightNavLine);
+    $('.bar6').addClass(rightNavLine);
+    $('.' + rightNavLine).css({"transform":"translateY(" + progress6 + "%)"});
   } else if (scrollValue < heightSec8) {
-    $('aside li').not('.bar7').removeClass('rightNav-line');
-    $('.bar7').addClass('rightNav-line');
-    $('.rightNav-line').css({"transform":"translateY(" + progress7 + "%)"});
+    $('aside li').not('.bar7').removeClass(rightNavLine);
+    $('.bar7').addClass(rightNavLine);
+    $('.' + rightNavLine).css({"transform":"translateY(" + progress7 + "%)"});
   } else {
-    $('aside li').not('.bar8').removeClass('rightNav-line');
-    $('.bar8').addClass('rightNav-line');
+    $('aside li').not('.bar8').removeClass(rightNavLine);
+    $('.bar8').addClass(rightNavLine);
+  }*/
+
+  if (rightNavLine === "line-index") {
+    console.log(rightNavLine);
+    if (scrollValue < heightSec2) {
+      $('aside li').not('.bar1').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar1').removeClass('line-black');
+      $('aside li').not('.bar1').removeClass('line-yellow');
+      $('.bar1').removeClass('line-yellow');
+      $('.bar1').attr('id', rightNavLine);
+      $('.bar1').addClass('line-black');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress1 + "%)"}); 
+      if (curSec2Height <= lineHeight.getBoundingClientRect().top) {
+        $('.bar1').removeClass('line-black');
+        $('.bar1').addClass('line-yellow');
+      }
+    } else if (scrollValue < heightSec3) {
+      $('aside li').not('.bar2').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar2').removeClass('line-black');
+      $('aside li').not('.bar2').removeClass('line-yellow');
+      $('.bar2').attr('id', rightNavLine);
+      $('.bar2').addClass('line-yellow');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress2 + "%)"});
+    } else if (scrollValue < heightSec4) {
+      $('aside li').not('.bar3').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar3').removeClass('line-black');
+      $('aside li').not('.bar3').removeClass('line-yellow');
+      $('.bar3').attr('id', rightNavLine);
+      $('.bar3').addClass('line-yellow');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress3 + "%)"});
+    } else if (scrollValue < heightSec5) {
+      $('aside li').not('.bar4').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar4').removeClass('line-black');
+      $('aside li').not('.bar4').removeClass('line-yellow');
+      $('.bar4').attr('id', rightNavLine);
+      $('.bar4').addClass('line-yellow');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress4 + "%)"});
+    } else if (scrollValue < heightSec6) {
+      $('aside li').not('.bar5').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar5').removeClass('line-black');
+      $('aside li').not('.bar5').removeClass('line-yellow');
+      $('.bar5').attr('id', rightNavLine);
+      $('.bar5').addClass('line-yellow');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress5 + "%)"});
+      if (curSec6Height <= lineHeight.getBoundingClientRect().top) {
+        $('.bar5').removeClass('line-yellow');
+        $('.bar5').addClass('line-black');
+      }
+    } else if (scrollValue < heightSec7) {
+      $('aside li').not('.bar6').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar6').removeClass('line-black');
+      $('aside li').not('.bar6').removeClass('line-yellow');
+      $('.bar6').attr('id', rightNavLine);
+      $('.bar6').addClass('line-black');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress5 + "%)"});
+      if (curSec7Height <= lineHeight.getBoundingClientRect().top) {
+        $('.bar6').removeClass('line-black');
+        $('.bar6').addClass('line-yellow');
+      }
+    } else if (scrollValue < heightSec8) {
+      $('aside li').not('.bar7').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar7').removeClass('line-black');
+      $('aside li').not('.bar7').removeClass('line-yellow');
+      $('.bar7').attr('id', rightNavLine);
+      $('.bar7').addClass('line-yellow');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress6 + "%)"});
+    } else {
+      $('aside li').not('.bar8').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar8').removeClass('line-black');
+      $('aside li').not('.bar8').removeClass('line-yellow');
+      $('.bar8').attr('id', rightNavLine);
+      $('.bar8').addClass('line-yellow');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress7 + "%)"});
+    }
+  } else {
+    if (scrollValue < heightSec2) {
+      $('aside li').not('.bar1').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar1').removeClass('line-black');
+      $('aside li').not('.bar1').removeClass('line-yellow');
+      $('.bar1').attr('id', rightNavLine);
+      $('.bar1').removeClass('line-yellow');
+      $('.bar1').addClass('line-black');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress1 + "%)"}); 
+    } else if (scrollValue < heightSec3) {
+      $('aside li').not('.bar2').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar2').removeClass('line-black');
+      $('aside li').not('.bar2').removeClass('line-yellow');
+      $('.bar2').attr('id', rightNavLine);
+      $('.bar2').removeClass('line-yellow');
+      $('.bar2').addClass('line-black');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress2 + "%)"}); 
+    } else if (scrollValue < heightSec4) {
+      $('aside li').not('.bar3').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar3').removeClass('line-black');
+      $('aside li').not('.bar3').removeClass('line-yellow');
+      $('.bar3').attr('id', rightNavLine);
+      $('.bar3').removeClass('line-yellow');
+      $('.bar3').addClass('line-black');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress3 + "%)"}); 
+    } else if (scrollValue < heightSec5) {
+      $('aside li').not('.bar4').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar4').removeClass('line-black');
+      $('aside li').not('.bar4').removeClass('line-yellow');
+      $('.bar4').attr('id', rightNavLine);
+      $('.bar4').removeClass('line-yellow');
+      $('.bar4').addClass('line-black');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress4 + "%)"});
+      if (curSec5Height <= lineHeight.getBoundingClientRect().top) {
+        $('.bar4').removeClass('line-black');
+        $('.bar4').addClass('line-yellow');
+      }
+    } else if (scrollValue < heightSec6) {
+      $('aside li').not('.bar5').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar5').removeClass('line-black');
+      $('aside li').not('.bar5').removeClass('line-yellow');
+      $('.bar5').attr('id', rightNavLine);
+      $('.bar5').addClass('line-yellow');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress5 + "%)"});
+      if (curSec5BotHeight <= lineHeight.getBoundingClientRect().top) {
+        $('.bar5').removeClass('line-yellow');
+        $('.bar5').addClass('line-black');
+      }
+    } else if (scrollValue < heightSec7) {
+      $('aside li').not('.bar6').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar6').removeClass('line-black');
+      $('aside li').not('.bar6').removeClass('line-yellow');
+      $('.bar6').attr('id', rightNavLine);
+      $('.bar6').addClass('line-black');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress5 + "%)"});
+    } else if (scrollValue < heightSec8) {
+      $('aside li').not('.bar7').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar7').removeClass('line-black');
+      $('aside li').not('.bar7').removeClass('line-yellow');
+      $('.bar7').attr('id', rightNavLine);
+      $('.bar7').addClass('line-black');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress6 + "%)"});
+      if (curSec7BotHeight <= lineHeight.getBoundingClientRect().top) {
+        $('.bar7').removeClass('line-black');
+        $('.bar7').addClass('line-yellow');
+      }
+    } else {
+      $('aside li').not('.bar8').removeAttr('id', rightNavLine);
+      $('aside li').not('.bar8').removeClass('line-black');
+      $('aside li').not('.bar8').removeClass('line-yellow');
+      $('.bar8').attr('id', rightNavLine);
+      $('.bar8').addClass('line-yellow');
+      $('#' + rightNavLine).css({"transform":"translateY(" + progress7 + "%)"});
+    }
   }
+
+  
   
 }
 
